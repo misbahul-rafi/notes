0985bd287bb782be36191d707cc73593cf33c0b0
misbahul-rafi <misbahulrafi24@gmail.com>
2024-10-07 06:53:43 +0000

    Feat: Setup OCR Service Production

--- docker/ocrservice/gunicorn.conf.py
+++
@@ -1,3 +0,0 @@
-accesslog = '/var/log/gunicorn/access.log'
-errorlog = '/var/log/gunicorn/error.log'
-loglevel = 'info'
--- docker/ocrservice/run.py
+++ docker/ocrservice/run.py
@@ -1,10 +1,4 @@
 from app import create_app
-from dotenv import load_dotenv
-
-load_dotenv()
-GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
-SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
-STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
 
 app = create_app()
 
